6d574ad173584f45369f0f3a84df2db66797b08c
unknown <kanec@my.lanecc.edu>
2018-04-08 16:17:06 -0700

    finished first version, unstyled

--- indexStartV1.js
+++ indexStartV1.js
@@ -16,24 +16,37 @@ var lines = [
 
 function init()
 {
-    // Add an onclick handler to all of the squares
-    // The name attribute for all of the divs is square
-    // Use the function handleClick to handle the event 
+    var cell;
+    for (var i = 0; i < 9; i++) {
+        cell = document.getElementsByTagName("button")[i];
+        cell.onclick = handleClick;
+    }  
+    // document.getElementsByTagName("button").onclick = handleClick;
+    // addEventListener("click", handleClick);
 }
 
 function handleClick() {
-
+    console.log("clicked");
     // Get the id from the square and put it in a variable
     // Remember that the id is an integer 0 - 8
-
+    var i = this.id;
     // Set the element in the squares array to the player's symbol
+    squares[i] = xIsNext ? 'X' : 'O';
     // Update the inner html for this square in the UI
+    this.innerHTML = squares[i];
     // Set the onclick handler for this square in the UI to an empty anonymous function or arrow function
+    this.onclick = function() {};
     // Update the variable xIsNext
-
+    xIsNext = !(xIsNext);
     // If calculateWinner returns true
     // highlight the winner and disable all of the squares
     // otherwise update the status in the UI to display the player
+    if (calculateWinner()) {
+        highlightWinner();
+    } else {
+        var status = document.querySelector("#status");
+        status.innerHTML = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
 }
 
 function calculateWinner() {
@@ -54,20 +67,26 @@ function calculateWinner() {
     return false;
 }
 
-//
 function highlightWinner() {
-
+    var status = document.querySelector("#status");
     // Update the status in the UI to display the winner
+    status.innerHTML = "Winner: " + winner;
     // Iterate through the winningLine array.  It contains the indices of the winning squares
     //      get the next square using the current index in the winningLine array as the id
     //      add the class red to the square
+    for (var i = 0; i < winningLine.length; i++) {
+        var cell = document.getElementById(winningLine[i]);
+        cell.classList.add("red"); 
+    }
     // Disable all of the squares
+    disableAll();
 }
 
 function disableAll() {
-
+    document.querySelectorAll(".col-xs-4 square").onclick = function() {};
     // Set the onclick handler for all squares to function that does nothing
     // The id of the square is a number 0 - 8
 }
 
-// When the page has finished loading, call the function init    
+// When the page has finished loading, call the function init  
+window.onload = init;
